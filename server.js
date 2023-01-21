83e2a072eb828220047261a6bbe1e696bc204cff
Anshi2304 <123232170+Anshi2304@users.noreply.github.com>
2023-01-21 13:47:45 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.send("Anshi - 166977215");
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
+
